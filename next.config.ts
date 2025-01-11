35cd3a4056f20bbf0f24c9e895869dd43f9849d0
ejinn1 <ejinn1@naver.com>
2025-01-11 15:24:36 +0900

    chore: add metadata to blog page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
+import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,7 +12,7 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
   },
 });
 
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,41 @@
+import { Metadata } from 'next';
+
+type BlogPageProps = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({
+  params,
+}: BlogPageProps): Promise<Metadata> {
+  const { meta } = await import(`@/content/${params.slug}.mdx`);
+
+  return {
+    title: meta.title,
+    description: meta.description,
+    openGraph: {
+      title: meta.title,
+      description: meta.description,
+      images: meta.image ? [meta.image] : [],
+    },
+  };
+}
+
 export default async function BlogPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
   const slug = (await params).slug;
-  const { default: Post } = await import(`@/content/${slug}.mdx`);
+  const { default: Post, meta } = await import(`@/content/${slug}.mdx`);
 
-  return <Post />;
+  console.log(meta);
+
+  return (
+    <div>
+      {/* <h1>{meta.title}</h1> */}
+      <Post />
+    </div>
+  );
 }
 
 export function generateStaticParams() {
