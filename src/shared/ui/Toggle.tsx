9c8394678b2ec1a9617fb918cd70c33027da0c46
ejinn1 <ejinn1@naver.com>
2025-01-29 16:38:39 +0900

    feat: add scroll top to close button

--- src/shared/ui/Toggle.tsx
+++ src/shared/ui/Toggle.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 
 interface ToggleProps {
   summary: string;
@@ -9,14 +9,23 @@ interface ToggleProps {
 
 const Toggle = ({ summary, children }: ToggleProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleRef = useRef<HTMLDivElement>(null);
 
   const toggleOpen = (e: React.MouseEvent) => {
     e.preventDefault();
     setIsOpen((prev) => !prev);
   };
 
+  const handleClose = () => {
+    if (toggleRef.current) {
+      toggleRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+    setIsOpen(false);
+  };
+
   return (
     <details open={isOpen} className="relative">
+      <div ref={toggleRef} className="absolute -top-80" />
       <summary onClick={toggleOpen} className="dark:text-gray-100">
         {summary}
       </summary>
@@ -24,7 +33,7 @@ const Toggle = ({ summary, children }: ToggleProps) => {
         <>
           <div className="pb-24 dark:text-gray-100">{children}</div>
           <button
-            onClick={toggleOpen}
+            onClick={handleClose}
             className="absolute bottom-0 right-0 rounded-4 bg-gray-100 px-4 dark:bg-gray-400 dark:text-gray-100"
           >
             닫기
