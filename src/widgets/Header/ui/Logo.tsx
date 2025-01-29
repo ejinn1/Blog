de382b9d4c4d46d8bf7c10c81a85dc6151a5c798
ejinn1 <ejinn1@naver.com>
2025-01-29 23:17:56 +0900

    chore: add google site meta

--- src/widgets/Header/ui/Logo.tsx
+++ src/widgets/Header/ui/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <div className="dark:text-beige-100 text-18 font-bold text-gray-200 md:text-20">
-      <Link href="/">메모장</Link>
+      <Link href="/">의진 블로그</Link>
     </div>
   );
 }
