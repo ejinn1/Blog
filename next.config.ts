ce295a711bb513022ccc0a120d74c908801f45fb
ejinn1 <ejinn1@naver.com>
2025-02-18 17:32:12 +0900

    setting: add rehype slug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 import rehypeHighlight from 'rehype-highlight';
+import rehypeSlug from 'rehype-slug';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
@@ -12,7 +13,7 @@ const withMDX = createMDX({
       remarkFrontmatter,
       [remarkMdxFrontmatter, { name: 'meta' }],
     ],
-    rehypePlugins: [rehypeHighlight],
+    rehypePlugins: [rehypeSlug, rehypeHighlight],
   },
 });
 
