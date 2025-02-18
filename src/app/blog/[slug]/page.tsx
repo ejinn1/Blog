840d525a500e073c8310fe8cc1bc4dc3c9ad35f8
ejinn1 <ejinn1@naver.com>
2025-02-18 17:37:10 +0900

    feat: add table of contents component to blog page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { getSlug } from '@/shared/lib/getSlug';
 import { CustomMetadata } from '@/shared/types/meta';
 import Tag from '@/shared/ui/Tag';
 import { getTags } from '@/shared/util/getTags';
+import TableOfContents from '@/widgets/TableOfContents';
 
 interface BlogPageProps {
   params: Promise<{ slug: string }>;
@@ -47,24 +48,27 @@ export default async function BlogPage({
   const tags = getTags(meta.keywords);
 
   return (
-    <div className="size-full py-24 md:w-750">
-      <article className="prose px-16 dark:prose-invert md:px-50">
-        <header className="border-b border-gray-100 py-16 dark:border-gray-400">
-          <h1 className="text-24 font-semibold">{meta.title}</h1>
-          <p className="text-16 font-medium text-gray-400 dark:text-gray-200">
-            {meta.description}
-          </p>
-          <div className="mt-16 flex items-center">
-            <p className="mr-20 text-12 text-gray-200">{meta.date}</p>
-            <div className="flex items-center gap-4 pb-4">
-              {tags.map((tag) => (
-                <Tag key={tag} tag={tag} />
-              ))}
-            </div>
+    <div className="size-full max-w-1000">
+      <header className="prose mx-auto w-full max-w-1000 border-b border-gray-100 p-16 dark:prose-invert dark:border-gray-400">
+        <h1 className="text-24 font-semibold">{meta.title}</h1>
+        <p className="text-16 font-medium text-gray-400 dark:text-gray-200">
+          {meta.description}
+        </p>
+        <div className="mt-16 flex items-center">
+          <p className="mr-20 text-12 text-gray-200">{meta.date}</p>
+          <div className="flex items-center gap-4 pb-4">
+            {tags.map((tag) => (
+              <Tag key={tag} tag={tag} />
+            ))}
           </div>
-        </header>
-        <Content />
-      </article>
+        </div>
+      </header>
+      <div className="flex size-full">
+        <article className="prose size-full px-16 py-24 dark:prose-invert md:w-750">
+          <Content />
+        </article>
+        <TableOfContents />
+      </div>
     </div>
   );
 }
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,7 +4,7 @@ import Navigation from './Navigation';
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 p-20 backdrop-blur-sm">
-      <div className="flex-between mx-auto max-w-750">
+      <div className="flex-between mx-auto max-w-1000">
         <Logo />
         <Navigation />
       </div>
