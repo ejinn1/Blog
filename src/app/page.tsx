21248280363c52abb834b3cadfb00c54dfe79331
ejinn1 <ejinn1@naver.com>
2025-02-18 17:37:10 +0900

    feat: add table of contents component to blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { PostSection } from '@/widgets/PostSection';
 
 export default async function Home() {
   return (
-    <div className="size-full py-24 md:w-750">
+    <div className="size-full py-24 md:w-1000">
       <IntroduceSection />
       <PostSection />
     </div>
