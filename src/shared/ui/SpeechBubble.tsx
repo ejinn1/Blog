25df926768d33f8a5aecf0bb9feff8cf2f08ba77
ejinn1 <ejinn1@naver.com>
2025-03-03 15:46:04 +0900

    refactor: improve frontend code

--- src/shared/ui/SpeechBubble.tsx
+++ src/shared/ui/SpeechBubble.tsx
@@ -7,8 +7,8 @@ interface SpeechBubbleProps {
 const SpeechBubble = ({ text }: SpeechBubbleProps) => {
   return (
     <div className="relative flex justify-start pl-8">
-      <div className="bg-beige-300 w-max rounded-[12px] p-12 text-sm font-semibold text-gray-400 dark:bg-gray-400 dark:text-gray-100">
-        <div className="border-t-beige-300 absolute -left-7 top-16 size-0 border-l-[15px] border-t-[15px] border-l-transparent dark:border-t-gray-400" />
+      <div className="w-max rounded-[12px] bg-beige-300 p-12 text-sm font-semibold text-gray-400 dark:bg-gray-400 dark:text-gray-100">
+        <div className="absolute -left-7 top-16 size-0 border-l-[15px] border-t-[15px] border-l-transparent border-t-beige-300 dark:border-t-gray-400" />
         {text}
       </div>
     </div>
