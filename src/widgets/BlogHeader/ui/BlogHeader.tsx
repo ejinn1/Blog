156059178efb5ad6499730a88e6b05e2c01032d8
ejinn1 <ejinn1@naver.com>
2025-03-05 17:39:17 +0900

    feat: add thumbnail image to blog page

--- src/widgets/BlogHeader/ui/BlogHeader.tsx
+++ src/widgets/BlogHeader/ui/BlogHeader.tsx
@@ -1,25 +1,38 @@
+import Image from 'next/image';
+
 import { CustomMetadata } from '@/shared/types/meta';
 import Tag from '@/shared/ui/Tag';
 
 interface BlogHeaderProps {
   meta: CustomMetadata;
   tags: string[];
+  slug: string;
 }
 
-const BlogHeader = ({ meta, tags }: BlogHeaderProps) => {
+const BlogHeader = ({ meta, tags, slug }: BlogHeaderProps) => {
+  const { title, description, date } = meta;
+
   return (
     <header className="prose mx-auto w-full max-w-1000 border-b border-gray-100 p-16 dark:prose-invert dark:border-gray-400">
-      <h1 className="text-24 font-semibold">{meta.title}</h1>
+      <h1 className="bottom-0 text-24 font-semibold">{title}</h1>
+      <Image
+        src={`/img/thumbnail/${slug}.png`}
+        width={100}
+        height={100}
+        layout="responsive"
+        quality={100}
+        className="rounded-12"
+        alt={`${title} thumbnail image`}
+      />
+
       <p className="text-16 font-medium text-gray-400 dark:text-gray-200">
-        {meta.description}
+        {description}
       </p>
-      <div className="mt-16 flex items-center">
-        <p className="mr-20 text-12 text-gray-200">{meta.date}</p>
-        <div className="flex items-center gap-4 pb-4">
-          {tags.map((tag) => (
-            <Tag key={tag} tag={tag} />
-          ))}
-        </div>
+      <p className="ml-4 text-12 text-gray-200">{date}</p>
+      <div className="flex items-center gap-4 pb-4">
+        {tags.map((tag) => (
+          <Tag key={tag} tag={tag} />
+        ))}
       </div>
     </header>
   );
