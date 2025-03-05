83c0fb5b2047b66cc8ff270817f9412a5a7f5366
ejinn1 <ejinn1@naver.com>
2025-03-05 15:19:37 +0900

    chore: change header logo to clover

--- src/widgets/Header/ui/Logo.tsx
+++ src/widgets/Header/ui/Logo.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <div className="dark:text-beige-100 text-18 font-bold text-gray-200 md:text-20">
-      <Link href="/">의진 블로그</Link>
+    <div className="text-18 font-bold text-gray-200 dark:text-beige-100 md:text-20">
+      <Link href="/">
+        <Image
+          src="/img/img-clover.png"
+          width="32"
+          height="32"
+          alt="logo"
+          className="transition-colors-base rounded-8 p-4 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-black dark:hover:text-gray-200"
+        />
+      </Link>
     </div>
   );
 }
